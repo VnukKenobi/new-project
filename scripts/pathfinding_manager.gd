6380fa84b903e3d67eb589925498da996f42e088
VnukKenobi <94792840+VnukKenobi@users.noreply.github.com>
2026-02-26 22:43:30 +0100

    Fix pathfinding API name collision with Node.get_path

---
+++ scripts/pathfinding_manager.gd
@@ -0,0 +1,30 @@
+class_name PathfindingManager
+extends Node
+
+var grid_manager: GridManager
+var astar := AStarGrid2D.new()
+
+func setup(in_grid_manager: GridManager) -> void:
+	grid_manager = in_grid_manager
+	rebuild()
+
+func rebuild() -> void:
+	astar.region = Rect2i(0, 0, grid_manager.GRID_WIDTH, grid_manager.GRID_HEIGHT)
+	astar.cell_size = Vector2.ONE
+	astar.diagonal_mode = AStarGrid2D.DIAGONAL_MODE_NEVER
+	astar.default_compute_heuristic = AStarGrid2D.HEURISTIC_MANHATTAN
+	astar.default_estimate_heuristic = AStarGrid2D.HEURISTIC_MANHATTAN
+	astar.update()
+
+	for blocked in grid_manager.blocked_cells.keys():
+		astar.set_point_solid(blocked, true)
+
+func has_path(start: Vector2i, goal: Vector2i) -> bool:
+	rebuild()
+	if astar.is_point_solid(start) or astar.is_point_solid(goal):
+		return false
+	return not astar.get_id_path(start, goal).is_empty()
+
+func find_path(start: Vector2i, goal: Vector2i) -> Array[Vector2i]:
+	rebuild()
+	return astar.get_id_path(start, goal)
