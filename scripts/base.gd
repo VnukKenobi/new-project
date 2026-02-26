e351c4e292fa29e76c7e3108ab357668a1158744
VnukKenobi <a3hudojnik@gmail.com>
2026-02-26 22:48:58 +0100

    Update base.gd

--- scripts/base.gd
+++ scripts/base.gd
@@ -10,6 +10,6 @@ func hit() -> void:
 	hp -= 1
 
 func _draw() -> void:
-	draw_ellipse(Vector2(0, 18), Vector2(18, 7), Color(0, 0, 0, 0.2))
+	draw_ellipse(Vector2(0, 18), Vector2(18, 7), Color(0.37, 0.37, 0.37, 0.2))
 	draw_rect(Rect2(Vector2(-20, -14), Vector2(40, 28)), Color.WHITE, true)
 	draw_rect(Rect2(Vector2(-10, -24), Vector2(20, 10)), Color.WHITE, true)
